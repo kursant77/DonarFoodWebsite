c0933fb197ecaa2b3086349e80df1ba788a6b9c6
Asadbek Jumanazarov <kursant410@gmail.com>
2025-10-31 12:56:16 +0500

    build fixed and ready for deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // ✅ Vercel uchun to‘g‘ri
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"), // 🔥 Bu juda muhim!
     },
   },
   build: {
