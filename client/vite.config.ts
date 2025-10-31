310349599fb3abf507ad46b503477b251d28ff08
Asadbek Jumanazarov <kursant410@gmail.com>
2025-10-31 13:13:29 +0500

    fix: Netlify SPA routing refresh bug

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/", // ⚠️ SPA uchun bu yerda "./" emas, "/" bo‘lishi kerak
   plugins: [react()],
   resolve: {
     alias: {
